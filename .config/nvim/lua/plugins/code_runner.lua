acb81a1da4f2d10bfcc5bb396b582bce2a1a4342
Alexis Lay Vu <alexislayvu@gmail.com>
2024-01-07 10:16:27 -0800

    edit keymap

--- .config/nvim/lua/plugins/code_runner.lua
+++ .config/nvim/lua/plugins/code_runner.lua
@@ -5,7 +5,7 @@ return {
     filetype_path = vim.fn.expand("~/.config/nvim/code_runner.json"),
     project_path = vim.fn.expand("~/.config/nvim/project_manager.json"),
     keys = {
-      { "<leader>r", ":RunCode<CR>", { noremap = true, silent = false, desc = "Run Code" } },
+      { "<leader>r", ":RunCode<CR>", desc = "Run Code" },
     },
   },
 }
