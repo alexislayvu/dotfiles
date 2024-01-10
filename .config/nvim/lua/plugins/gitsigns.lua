2cef35c632c978afb21700e1083474d72fb6fd41
Alexis Lay Vu <alexislayvu@gmail.com>
2024-01-10 12:47:16 -0800

    update gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -7,6 +7,9 @@ return {
       gitsigns.setup()
       scrollbar_gitsigns.setup()
     end,
-    opts = { current_line_blame = true, current_line_blame_formatter = "<author>," },
+    opts = {
+      current_line_blame = true,
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> · <summary>",
+    },
   },
 }
