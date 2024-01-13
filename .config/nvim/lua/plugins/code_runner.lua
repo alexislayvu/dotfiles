1a24fa5b4035db75e9467e52bd1dc5e9988eb74e
Alexis Lay Vu <alexislayvu@gmail.com>
2024-01-12 21:54:15 -0800

    updates...

--- .config/nvim/lua/plugins/code_runner.lua
+++ .config/nvim/lua/plugins/code_runner.lua
@@ -5,7 +5,7 @@ return {
     filetype_path = vim.fn.expand("~/.config/nvim/code_runner.json"),
     project_path = vim.fn.expand("~/.config/nvim/project_manager.json"),
     keys = {
-      { "<leader>r", ":RunCode<CR>", desc = "Run Code" },
+      { "<leader>r", "<cmd>RunCode<cr>", desc = "Run Code" },
     },
   },
 }
--- .config/nvim/lua/plugins/undotree.lua
+++ .config/nvim/lua/plugins/undotree.lua
@@ -2,7 +2,7 @@ return {
   {
     "mbbill/undotree",
     keys = {
-      { "<leader>ue", ":UndotreeToggle<CR>:UndotreeFocus<CR>", desc = "Toggle Undotree" },
+      { "<leader>ue", "<cmd>UndotreeToggle<cr><cmd>UndotreeFocus<cr>", desc = "Toggle Undotree" },
     },
   },
 }
