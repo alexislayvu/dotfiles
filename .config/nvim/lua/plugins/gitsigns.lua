ace11feb2f31de448490cede8ac4024abd459472
Alexis Lay Vu <alexislayvu@gmail.com>
2024-01-23 02:02:17 -0800

    update current line blame formatter

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -9,7 +9,7 @@ return {
     end,
     opts = {
       current_line_blame = true,
-      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> · <summary>",
     },
   },
 }
