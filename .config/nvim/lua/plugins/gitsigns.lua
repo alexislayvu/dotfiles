ffc2fdabdfc125bf51897fb37a0fd178da01725a
Alexis Lay Vu <alexislayvu@gmail.com>
2024-01-22 19:35:35 -0800

    fix git line blame not showing

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,15 +1,15 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    config = function()
+    config = function(_, opts)
       local gitsigns = require("gitsigns")
       local scrollbar_gitsigns = require("scrollbar.handlers.gitsigns")
-      gitsigns.setup()
+      gitsigns.setup(opts)
       scrollbar_gitsigns.setup()
     end,
     opts = {
       current_line_blame = true,
-      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> · <summary>",
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
     },
   },
 }
