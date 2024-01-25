8f47e71fcc6816827bd55af2cac4f4a6193bebd1
Alexis Lay Vu <alexislayvu@gmail.com>
2024-01-24 19:33:00 -0800

    updates

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,10 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
+      -- options = {
+      --   component_separators = { left = "|", right = "|" },
+      --   section_separators = { left = "█", right = "█" },
+      -- },
       sections = {
         lualine_z = {
           function()
